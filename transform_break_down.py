db685e3b514da9297c24e8b27f5758598d765a7d
jueliu <jue57ks123@gmail.com>
2022-12-04 23:58:19 +0800

    animation complete
    1. demo: rotate first, then translate'
    2. demo: one arbitrary rotate = three elementary rotation

--- transform_break_down.py
+++ transform_break_down.py
@@ -8,12 +8,12 @@ import time
 import copy
 from itertools import chain
 
-def get_axis():
+def get_axis(size=0.3):
     points = [
                 [0, 0, 0],
-                [0.5, 0, 0],
-                [0, 0.5, 0],
-                [0, 0, 0.5],
+                [size, 0, 0],
+                [0, size, 0],
+                [0, 0, size],
             ]
     lines = [
         [0, 1],
@@ -30,11 +30,18 @@ def get_axis():
 
 def create_vehicle():
     body_frame = get_axis()
-    body_frame.translate(np.array([2,0,0]))
     obj = o3d.geometry.TriangleMesh.create_cone(radius=0.05, height=0.15,create_uv_map=True)
-    obj.translate(np.array([2.0,0,0.1]))
+    obj.translate(np.array([0,0,0.1]))
+
     return obj, body_frame
 
+def to_homo(*, rot=None, trans=None):
+    homo = np.eye(4)
+    if rot is not None:
+        homo[:3,:3] = rot
+    if trans is not None:
+        homo[:3,3] = trans
+    return homo
 
 def get_homo_rot(rot_matrix):
     homo = np.eye(4)
@@ -71,152 +78,209 @@ def get_homo_quaternion_matrix(q):
     return get_homo_rot(T), T
 
 def get_pose_mat(quat, position):
-    T = Rotation.from_quat(quat).as_matrix()
+    T = get_homo_rot(Rotation.from_quat(quat).as_matrix())
     T[0,3] = position[0]
     T[1,3] = position[1]
     T[2,3] = position[2]
     return T
 
+def inter_keyframe(pose1, pose2):
+    '''
+    get pose P which pose1 -> P -> pose2 are rot and trans individually
+    '''
+    pose1_pose2_tf = np.dot(pose2, np.linalg.inv(pose1))
+    _1to2_quat = Rotation.from_matrix(pose1_pose2_tf[:3,:3]).as_quat()
 
-def main():
+    another_1to2_quat = Rotation.from_matrix(np.dot(pose2[:3,:3],np.linalg.inv(pose1[:3,:3]))).as_quat()
 
-    rot1, rot1_3x3 = get_rotation_matrix(45, 45, 45)
-    rot2, rot2_3x3 = get_rotation_matrix(90, 45, 30)
-    T = get_homo_translate_matrix(1,1,1)
+    rot_only = copy.deepcopy(pose1_pose2_tf)
+    rot_only[0,3] = 0
+    rot_only[1,3] = 0
+    rot_only[2,3] = 0
+    keyframe = np.dot(rot_only, pose1) 
+    return keyframe
 
-    rot2_part1, _ = get_rotation_matrix(0,0,30)
-    rot2_part2, _ = get_rotation_matrix(0,45,0)
-    rot2_part3, _ = get_rotation_matrix(90,0,0)
-    concate = np.dot(rot2_part1, np.dot(rot2_part2, rot2_part3))
-    #print(concate)
-    #print(rot2)
+def rot_animation(pose1, pose2, interval=30):
 
-    rot2_forward1 = rot2_part3
-    rot2_forward2 = np.dot(rot2_part2, rot2_forward1)
-    rot2_forward3 = np.dot(rot2_part1, rot2_forward2)
-    #print(rot2_forward3)
+    # check only rotation happen
+    diff_tf = np.dot(pose2, np.linalg.inv(pose1))
+    assert(abs(diff_tf[0,3]) < 1e-15)
+    assert(abs(diff_tf[1,3]) < 1e-15)
+    assert(abs(diff_tf[2,3]) < 1e-15)
 
-    inv_z, _ = get_ele_rotation_matrix('z',-90)
-    inv_y, _ = get_ele_rotation_matrix('y', -45)
-    inv_x, _ = get_ele_rotation_matrix('x', -30)
+    diff_rotvec = Rotation.from_matrix(diff_tf[:3,:3]).as_rotvec()
+    rot_step = Rotation.from_rotvec(diff_rotvec/float(interval))
 
-    rot2_back1 = np.dot(inv_x, rot2)
-    rot2_back2 = np.dot(inv_y, rot2_back1)
-    rot2_back3 = np.dot(inv_z, rot2_back2)
-    print(rot2_back3)
-    
-    orig_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1)
+    inter_pose = pose1
+    for i in range(interval):
+        inter_pose = np.dot(get_homo_rot(rot_step.as_matrix()), inter_pose)
+        yield inter_pose, None
+    #print('pose the same?', pose2, inter_pose)
 
-    obj, obj_axis = create_vehicle()
-    #obj.transform(rot2)
-    #obj_axis.transform(rot2)
-    #o3d.visualization.draw_geometries([orig_frame, obj, obj_axis])
+def trans_animation(pose1, pose2, interval=30):
 
-    qrot = Rotation.from_matrix(rot2_3x3).as_quat()
-    qrot2 = Rotation.from_matrix(rot1_3x3).as_quat()
+    # TODO
+    # check only translation happen
 
+    diff_tf = np.dot(pose2, np.linalg.inv(pose1))
+    trans_step = np.array(diff_tf[0:3,3])/float(interval) # copy
+    tf_step = np.eye(4)
+    tf_step[0:3,3] = trans_step
+
+    inter_pose = pose1
+    for i in range(interval):
+        inter_pose = np.dot(tf_step, inter_pose)
+
+        yield inter_pose, None
+
+def never_closed_visualizer(geometry_list):
     vis = o3d.visualization.Visualizer()
     vis.create_window()
-    vis.add_geometry(orig_frame)
-    vis.add_geometry(obj)
-    vis.add_geometry(obj_axis)
-    end_obj = copy.deepcopy(obj)
-    end_obj_axis = copy.deepcopy(obj_axis)
-    end_obj.transform(rot2)
-    end_obj_axis.transform(rot2)
-    vis.add_geometry(end_obj)
-    vis.add_geometry(end_obj_axis)
-    vis.poll_events()
-    vis.update_renderer()
-    
-    #cos = qrot[0]
-    #sin = LA.norm(qrot[1:])
-    #angle = math.atan(sin/cos)
-    #print('full angle is', angle * 180 / np.pi)
-    #rot_axis = qrot[1:] / sin
-    ##print(LA.norm(qrot), sum(i*i for i in qrot[1:]), qrot[0]*qrot[0])
+    for g in geometry_list:
+        vis.add_geometry(g)
 
-
-    def slerp(quat, interval=100):
-        # calc end quat angle and axis
-        print('original quat:', quat)
-        cos = qrot[3]
-        sin = LA.norm(qrot[0:3])
-        angle = math.atan(sin/cos)
-        print('full angle is', angle * 180 / np.pi)
-        rot_axis = qrot[0:3] / sin
-
-        for i in range(0, interval):
-            angle_step = angle *i /interval 
-            print('angle step=', angle_step * 180 / np.pi)
-            qrot_step = np.zeros((4,))
-            qrot_step[3] = math.cos(angle_step)
-            qrot_step[0:3] = rot_axis * math.sin(angle_step)
-            #print(LA.norm(qrot_step))
-            rot_step_matrix, _ = get_homo_quaternion_matrix(qrot_step)
-            print(i)
-            print(qrot_step)
-            print(rot_step_matrix)
-            yield rot_step_matrix
-
-    '''
-    ref: https://dl.acm.org/doi/pdf/10.1145/325334.325242
-    '''
-    def another_slerp(quat1, quat2, interval=100):
-        quat1 = np.array(quat1)
-        quat2 = np.array(quat2)
-        cos_theta = np.inner(quat1, quat2) 
-        sin_theta = math.sqrt(1-cos_theta*cos_theta)
-        theta = math.atan(sin_theta/cos_theta)
-
-        for u in np.arange(0, 1, float(1./interval)):
-            new_quat = math.sin(theta*(1.-u))/sin_theta * quat1 + math.sin(theta*u)/sin_theta * quat2
-            rot_mat, _ = get_homo_quaternion_matrix(new_quat)
-            yield rot_mat 
-
-    
-
-    #interpolation_tf_iterator = slerp(qrot, 100)
-    #interpolation_tf_iterator_1 = another_slerp([0,0,0,1], qrot, 100)
-    #interpolation_tf_iterator_2 = another_slerp(qrot, qrot2, 100)
-    qrot_forward1 = Rotation.from_matrix(rot2_forward1[:3,:3]).as_quat()
-    qrot_forward2 = Rotation.from_matrix(rot2_forward2[:3,:3]).as_quat()
-    qrot_forward3 = Rotation.from_matrix(rot2_forward3[:3,:3]).as_quat()
-    qrot_back1 = Rotation.from_matrix(rot2_back1[:3,:3]).as_quat()
-    qrot_back2 = Rotation.from_matrix(rot2_back2[:3,:3]).as_quat()
-    qrot_back3 = Rotation.from_matrix(rot2_back3[:3,:3]).as_quat()
-
-    forward_tf_1 = another_slerp([0,0,0,1], qrot_forward1)
-    forward_tf_2 = another_slerp(qrot_forward1, qrot_forward2)
-    forward_tf_3 = another_slerp(qrot_forward2, qrot_forward3)
-    reverse_tf_1 = another_slerp(qrot, qrot_back1)
-    reverse_tf_2 = another_slerp(qrot_back1, qrot_back2)
-    reverse_tf_3 = another_slerp(qrot_back2, qrot_back3)
-    #all_iter = chain(interpolation_tf_iterator_1, reverse_tf_1, reverse_tf_2, reverse_tf_3)
-    all_iter = chain(forward_tf_1, forward_tf_2, forward_tf_3, reverse_tf_1, reverse_tf_2, reverse_tf_3)
-
-    for i in range(600):
-        rot_step_matrix = next(all_iter)
-        #input()
-        obj.transform(rot_step_matrix)
-        obj_axis.transform(rot_step_matrix)
-        vis.update_geometry(obj)
-        vis.update_geometry(obj_axis)
-        #vis.update_geometry(orig_frame)
+    while True:
         vis.poll_events()
         vis.update_renderer()
-        time.sleep(0.1)
-        #orig_frame.transform(np.linalg.inv(rot_step_matrix))
-        obj.transform(np.linalg.inv(rot_step_matrix))
-        obj_axis.transform(np.linalg.inv(rot_step_matrix))
-    vis.destroy_window()
-    o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Info)
-    return
 
+
+def animation_display(static_geoms, animations):
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+
+    all_iter = chain(*animations)
+    moving_obj = get_axis(size=0.15)
+
+    for g in static_geoms:
+        vis.add_geometry(g)
+    vis.add_geometry(moving_obj)
+
+    pose_save = []
+    cache_added_obj = []
+
+    while True:
+
+        try:
+            anim_pose, _ = next(all_iter)
+            pose_save.append((anim_pose, None))
+            moving_obj.transform(anim_pose)
+            vis.update_geometry(moving_obj)
+            vis.poll_events()
+            vis.update_renderer()
+
+            anim_trace = copy.deepcopy(moving_obj)
+            cache_added_obj.append(anim_trace)
+            vis.add_geometry(anim_trace, reset_bounding_box=False)
+            moving_obj.transform(np.linalg.inv(anim_pose))
+            time.sleep(0.01)
+        except StopIteration:
+            for i in range(100):
+                vis.poll_events()
+                vis.update_renderer()
+                time.sleep(0.01)
+            all_iter = iter(pose_save) 
+            pose_save = []
+            for a in cache_added_obj:
+                vis.remove_geometry(a, reset_bounding_box=False)
+            cache_added_obj = []
+            
+
+def animation_persist_display(static_geoms, animations):
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+
+    all_iter = chain(*animations)
+    moving_obj = get_axis(size=0.15)
+
+    for g in static_geoms:
+        vis.add_geometry(g)
+    vis.add_geometry(moving_obj)
+
+    while True:
+
+        anim_pose, _ = next(all_iter)
+        moving_obj.transform(anim_pose)
+        vis.add_geometry(copy.deepcopy(moving_obj))
+        #vis.update_geometry(moving_obj)
+        vis.poll_events()
+        vis.update_renderer()
+        moving_obj.transform(np.linalg.inv(anim_pose))
+        time.sleep(0.01)
+
+def main3():
+
+    # scene setup
+    orig_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1)
+    quat1 = Rotation.from_euler('zyx', [45,45,45]).as_quat()
+    quat2 = Rotation.from_euler('zyx', [10, 45, 30]).as_quat()
+    quat3 = Rotation.from_euler('zyx', [20,45,15]).as_quat()
+
+    orig_pose = get_pose_mat([0,0,0,1], [0,0,0])
+    key_pose0 = get_pose_mat([0,0,0,1], [1, 0.5, 0.5])
+    key_pose1 = get_pose_mat(quat1, [2, 1, 1])
+    key_pose2 = get_pose_mat(quat2, [4, 2, 2])
 
     straight_route = o3d.geometry.LineSet(
-            points=o3d.utility.Vector3dVector([[-10, -10, -10], [10, 10, 10]])
+            points=o3d.utility.Vector3dVector([[0,0,0], [6,3,3]]),
+            lines=o3d.utility.Vector2iVector([[0,1]]),
             )
 
+    pose0_axis = get_axis().transform(key_pose0)
+    pose1_axis = get_axis().transform(key_pose1)
+    pose2_axis = get_axis().transform(key_pose2)
+
+    # animation 1
+    rot_anim_keypose1 = rot_animation(key_pose0, inter_keyframe(key_pose0, key_pose1))
+    trans_anim_1 = trans_animation(inter_keyframe(key_pose0, key_pose1), key_pose1)
+
+    # animation 2
+    rot_anim_keypose2 = rot_animation(key_pose1, inter_keyframe(key_pose1, key_pose2))
+    trans_anim_2 = trans_animation(inter_keyframe(key_pose1, key_pose2), key_pose2)
+
+    animation_display([orig_frame, straight_route, pose0_axis, pose1_axis, pose2_axis], [rot_anim_keypose1, trans_anim_1, rot_anim_keypose2, trans_anim_2])
+
+
+def main4():
+
+    orig_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1)
+
+    def transform_all(geoms, tf):
+        return [g.transform(tf) for g in geoms]
+
+    # init obj pose
+    init_translate = to_homo(trans=np.array([2,0,0]))  
+    rot, rot_3x3 = get_rotation_matrix(90, 45, 30) 
+    rot_part1, _ = get_rotation_matrix(0,0,30)
+    rot_part2, _ = get_rotation_matrix(0,45,0)
+    rot_part3, _ = get_rotation_matrix(90,0,0)
+    print(rot)
+    print(np.dot(rot_part1, np.dot(rot_part2, rot_part3)))
+
+    old_obj, old_obj_axis = create_vehicle()
+    old_obj.transform(init_translate)
+    old_obj_axis.transform(init_translate)
+
+    new_obj, new_obj_axis = create_vehicle()
+    new_obj.transform(init_translate).transform(rot)
+    new_obj_axis.transform(init_translate).transform(rot)
+
+
+    # keyframe
+    inter_pose1 = np.dot(rot_part3, init_translate)
+    inter_pose2 = np.dot(rot_part2, inter_pose1)
+    inter_pose3 = np.dot(rot_part1, inter_pose2)
+
+    # animation
+    anim_keypose1 = rot_animation(init_translate, inter_pose1)
+    anim_keypose2 = rot_animation(inter_pose1, inter_pose2)
+    anim_keypose3 = rot_animation(inter_pose2, inter_pose3)
+    all_anim = chain(anim_keypose1, anim_keypose2, anim_keypose3)
+
+    animation_display([orig_frame, old_obj, old_obj_axis, new_obj, new_obj_axis], [anim_keypose1, anim_keypose2, anim_keypose3])
+    #animation_persist_display([orig_frame, new_obj, new_obj_axis], [anim_keypose1, anim_keypose2, anim_keypose3])
+
+
+    return
 if __name__ == "__main__":
-    main()
+    main3()
+    #main4()
